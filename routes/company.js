f94b29116b2c6ba05ee465a8ed1cf3013812919a
kndTrevy <kndtrevy@gmail.com>
2021-04-06 16:54:15 +0200

    Modiification de la route company

--- routes/company.js
+++ routes/company.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const { requireSignin, customerMiddleware, adminMiddleware } = require('../common-middleware');
 const { createCompany, disableCompany, enableCompany, getCompany, getOneCompany, deleteCompany } = require('../controllers/company');
+const multer = require('multer');
 const router = express.Router();
 
-router.post('/create/company', requireSignin, createCompany);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+});
+
+const upload = multer({ storage });
+
+router.post('/create/company', requireSignin, upload.single('image'), createCompany);
 router.post('/disable/company', requireSignin, adminMiddleware, disableCompany);
 router.post('/enable/company', requireSignin, adminMiddleware, enableCompany);
 router.post('/delete/company', requireSignin, adminMiddleware, deleteCompany);
